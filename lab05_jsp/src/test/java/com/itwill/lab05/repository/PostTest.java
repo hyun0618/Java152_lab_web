a952a35d3f6a37338ef7802d8de0951c5df3cbcd
hyun0618 <jhh48654@gmail.com>
2024-05-27 17:45:16 +0900

    Post entity, PostDao JUnit test

--- lab05_jsp/src/test/java/com/itwill/lab05/repository/PostTest.java
+++ lab05_jsp/src/test/java/com/itwill/lab05/repository/PostTest.java
@@ -1,5 +1,7 @@
 package com.itwill.lab05.repository;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -8,9 +10,11 @@ import org.slf4j.LoggerFactory;
 public class PostTest {
 	private static final Logger log = LoggerFactory.getLogger(PostTest.class);
 	
+	private PostDao dao = PostDao.INSTANCE;
+	
 	// JUnit 모듈에서 단위 테스트를 하기 위해서 호출하는 메서드. 
 	// (1) public void. (2) 아규먼트를 갖지 않음.  
-	@Test
+//	@Test
 	public void test() {
 		// Post 타입 객체 생성 - Builder 디자인 패턴. 
 		Post p = Post.builder()
@@ -23,8 +27,16 @@ public class PostTest {
 		// assertNotNull(arg): arg가 null이 아니면 JUnit 테스트 성공, null이면 테스트 실패. 
 		// assertNull(arg): arg가 null이면 단위 테스트 성공, null이 아니면 테스트 실패.
 		Assertions.assertNotNull(p);
-		log.debug("p = {} ", p);
-				
+		log.debug("p = {}", p);		
+	}
+	
+	@Test
+	public void testPostDao() {
+		Assertions.assertNotNull(dao); // PostDao 타입 객체가 null이 아니면 단위 테스트 성공.
+		log.debug("dao = {}", dao);
+		
+		List<Post> result = dao.select();
+		Assertions.assertNull(result);
 	}
 
 }
