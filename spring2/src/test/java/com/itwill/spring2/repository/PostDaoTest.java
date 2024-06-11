3acdfe276c53881305e23baa8878850d9847ad0c
hyun0618 <jhh48654@gmail.com>
2024-06-11 20:08:31 +0900

    .

--- spring2/src/test/java/com/itwill/spring2/repository/PostDaoTest.java
+++ spring2/src/test/java/com/itwill/spring2/repository/PostDaoTest.java
@@ -18,16 +18,56 @@ import lombok.extern.slf4j.Slf4j;
 )
 public class PostDaoTest {
 	
-	@Autowired
+	@Autowired // 의존성 주입(Dependency Injection)
 	private PostDao postDao;
 	
-	@Test
+//	@Test
 	public void testSelectAll() {
 		Assertions.assertNotNull(postDao);
 		
 		List<Post> list = postDao.selectOrderByIdDesc();
 		for (Post p : list) {
-			System.out.println(p);
+			System.out.println("\t*****\t" + p);
 		}
 	}
+	
+//	@Test
+	public void testSelectById() {
+		Post post1 = postDao.selectById(35); // DB테이블에 id가 있는 경우
+		Assertions.assertNotNull(post1);
+		log.debug(post1.toString());
+		
+		Post post2 = postDao.selectById(1); // DB테이블에 id가 없는 경우
+		Assertions.assertNull(post2);
+	}
+	
+//	@Test
+	public void testInsertPost() {
+		Post post = Post.builder()
+				.title("MyBatis 테스트")
+				.content("MyBatis-Spring insert 테스트")
+				.author("admin")
+				.build();
+		int result = postDao.insertPost(post);
+		Assertions.assertEquals(1, result);
+	}
+	
+	@Test
+	public void testUpdatePost() {
+		// 업데이트 할 포스트 객체 만들기:
+		Post post = Post.builder()
+				.id(41)
+				.title("modifiedTime")
+				.content("modifiedTime 업데이트")
+				.build(); // modifiedTime은 입력할 필요가 없다. 
+		int result = postDao.updatePost(post);
+		Assertions.assertEquals(1, result);
+	}
+	
+//	@Test
+	public void testDeletePost() {
+		int result = postDao.deletePost(42);
+		Assertions.assertEquals(1, result);
+	}
+	
 }
