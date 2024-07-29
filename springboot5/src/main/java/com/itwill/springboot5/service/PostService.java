ae122a4841c24a79a26146e29fb4fc348525aa13
hyun0618 <jhh48654@gmail.com>
2024-07-29 18:13:09 +0900

    post create, list, details

--- springboot5/src/main/java/com/itwill/springboot5/service/PostService.java
+++ springboot5/src/main/java/com/itwill/springboot5/service/PostService.java
@@ -1,5 +1,52 @@
 package com.itwill.springboot5.service;
 
-public class PostService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.itwill.springboot5.domain.Post;
+import com.itwill.springboot5.dto.PostCreateDto;
+import com.itwill.springboot5.dto.PostListItemDto;
+import com.itwill.springboot5.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PostService {
+	
+	private final PostRepository postRepo;
+	
+	public List<PostListItemDto> read() {
+		log.info("read()");
+		// 영속성(persistence/repository) 계층의 메서드를 호출해서 엔터티들의 리스트를 가져옴. 
+		List<Post> list = postRepo.findAll();
+		log.info("list size={}", list.size());
+		
+		// List<Post>를 List<PostListItemDto> 타입으로 변환.		
+		return list.stream()
+				.map(PostListItemDto::fromEntity) // (x) -> PostListItemDto.fromEntity(x)
+				.toList();	
+		
+//		List<PostListItemDto> posts = list.stream()
+//				.map(PostListItemDto::fromEntity)
+//				.toList();
+//		return posts;	
+	}
+	
+	public void save(PostCreateDto dto) {
+        log.info("save(dto={})", dto);
+        
+        Post post = dto.toEntity();
+        postRepo.save(post);
+    }
+	
+	public Post read(Long id) {
+		return postRepo.findById(id).orElseThrow();
+	}
+	
+
+ 	
 }
--- springboot5/src/main/java/com/itwill/springboot5/web/PostController.java
+++ springboot5/src/main/java/com/itwill/springboot5/web/PostController.java
@@ -1,21 +1,61 @@
 package com.itwill.springboot5.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.itwill.springboot5.domain.Post;
+import com.itwill.springboot5.dto.PostCreateDto;
+import com.itwill.springboot5.dto.PostListItemDto;
+import com.itwill.springboot5.service.PostService;
+
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller @RequestMapping("/post")
 public class PostController {
 	
+	private final PostService postSvc;
+	
 	@GetMapping("/list")
-	public void list() {
+	public void list(Model model) {
 		
 		log.info("list()");
-		// TODO: 서비스 계층의 메서드를 호출 --> 뷰에 포스트 목록 전달 
+		// 서비스 계층의 메서드를 호출 --> 뷰에 포스트 목록 전달 
+		List<PostListItemDto> list = postSvc.read();
+		model.addAttribute("posts", list);
+	}
+	
+	@GetMapping("/create")
+	public void create() {
 		
+	}
+	
+	@PostMapping("/create")
+    public String save(@RequestParam String title, @RequestParam String content, @RequestParam String author) {
+        PostCreateDto dto = new PostCreateDto();
+        dto.setTitle(title);
+        dto.setContent(content);
+        dto.setAuthor(author);
+        postSvc.save(dto);
+        return "redirect:/post/list";
+    }
+	
+	@GetMapping("/details/{id}")
+	public String list(@PathVariable Long id, Model model) {
+		Post post = postSvc.read(id);
+		model.addAttribute("post", post);
+		return "post/details";
 	}
 	
 }
