d3cb559bff810a06f18f71ba69f6163a30e7dd56
hyun0618 <jhh48654@gmail.com>
2024-08-01 16:23:58 +0900

    test

--- springboot5/src/test/java/com/itwill/springboot5/repository/PostQuerydslTest.java
+++ springboot5/src/test/java/com/itwill/springboot5/repository/PostQuerydslTest.java
@@ -6,8 +6,13 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.itwill.springboot5.domain.Post;
+import com.itwill.springboot5.dto.PostSearchRequestDto;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,7 +35,25 @@ public class PostQuerydslTest {
 //		result = postRepo.searchByTitle("DUMM");
 //		result = postRepo.searchByContent("tes");
 //		result = postRepo.searchByTitleOrContent("tes");
-		result = postRepo.searchByModifiedTime(LocalDateTime.of(2024, 7, 29, 12, 0), LocalDateTime.of(2024, 7, 30, 12, 0));
+//		result = postRepo.searchByModifiedTime(LocalDateTime.of(2024, 7, 29, 12, 0), LocalDateTime.of(2024, 7, 30, 12, 0));
+		
+//		LocalDateTime from = LocalDateTime.of(2024, 7, 29, 0, 0);
+//		LocalDateTime to = LocalDateTime.of(2024, 7, 29, 23, 59);
+//		result = postRepo.searchByModifiedTime(from, to);
+		
+//		result = postRepo.searchByAuthorAndTitle("guest", "1");
+		
+//		PostSearchRequestDto dto = new PostSearchRequestDto();
+//		dto.setCategory("tc");
+//		dto.setKeyword("dum title");
+//		result = postRepo.searchByCategory(dto);
+		
+		String[] keywords = "ad date".split(" "); // {"dum", "title"};
+//		result = postRepo.searchByKeywords(keywords);
+		
+		Pageable pageable = PageRequest.of(0, 5, Sort.by("modifiedTime").descending());
+		Page<Post> page = postRepo.searchByKeywords(keywords, pageable);
+		page.forEach(System.out::println);
 		
 		// result 전체 출력. 
 		result.forEach(System.out::println);
